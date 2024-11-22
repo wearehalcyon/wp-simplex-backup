18d27720208845863484e3419f5335ff367901bb
ArtRomanWorx <thewitness45@gmail.com>
2024-11-22 16:10:14 +0200

    init

---
+++ wp-simplex-backup.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin name: WP SimpleX Backup & Migration
+ * Version: 1.0.0
+ * Author: INTAKE Digital
+ * Author URI: https://intakedigital.net/
+ * Description: This is simple and fast plugin for making backups for your web-site and also providing migration from one to second.
+ * @license GPL-3.0 license
+ * License: GPL-3.0
+ * Network: true
+ * Requires PHP: 8.2
+ * Requires at least: 6.7.1
+ * GitHub Plugin URI: https://github.com/wearehalcyon/wp-simplex-backup
+ * Primary Branch: main
+ */
